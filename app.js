12865425f10a9a9b24c40dc8289bbc97fff72e2e
UZAIR SHAH <137003543+uzishah@users.noreply.github.com>
2023-10-21 21:25:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,300 @@
+//mathmatical expression
+//qo.1
+
+// var a =3
+// var b =5
+// sum = a+b
+// document.write( "the sum of a and b is"+sum)
+
+//qno.2
+// var a =3
+// var b =5
+// pro = a*b
+// document.write( "the product of a and b is"+pro)
+// var a =3
+// var b =5
+// div= a/b
+// document.write( "the division of a and b is"+div)
+
+// var a =3
+// var b =5
+// sub = a-b
+// document.write( "the substraction  of a and b is"+sub)
+
+// qno.3
+
+// var a = 10
+// document.write( "the value of a is"+a)
+// a++
+// document.write("the value after increment"+a)
+// a=a+7
+// document.write("after adding 7 to the previous value "+a)
+// a--
+// document.write("the value after decrement"+a)
+// a= a/3
+// document.write("value after dividing by 3 "+a)
+// var a = 2
+// for( i=1; i<=10; i++){
+//     product =a*i
+//     document.write(product)
+// }
+//qno.7
+
+// var obt=250
+
+// var tot=1000
+// persentage = (obt/tot)*100
+// document.write(persentage)
+
+// var usd = 10
+// var riyal = 20
+// var usdTopkr = 208
+// var riyalTopkr = 47
+// total = (usd*usdTopkr)+(riyal*riyalTopkr)
+// document.write(total)
+
+
+// var a= 10
+// total = ((a+5)*10 )/2
+// document.write(total)
+
+//age calculator
+
+// var b_year= 1993
+// var crrunt_year = 2023
+// var age = crrunt_year - b_year
+// document.write(age)
+
+
+// var fvrtSnack = choclateChips
+// document.write( "my favrt snack is "+fvrtSnack)
+
+// var crrntAge=20
+// document.write("my age is "+crrntAge)
+// var maxAge=55
+// document.write("my max age will be"+maxAge)
+// var perDay = 3
+// document.write("eat per day snake"+perDay)
+// var totalDays = (maxAge-crrntAge)/365
+// document.write("in how many days can eat my favorite snacks in a year"+totalDays)
+// var age= maxAge-crrntAge
+// document.write("how much years left till I reach my max age"+age)
+// var snakes_eaten = age*totalDays*3
+// document.write("How many snacks of my favourite I'll eat in the next few years" +snakes_eaten)
+
+// var a = 10
+// document.write("the value of a is"+a)
+// a++
+// document.write("<br>after incrementing by one, the new value of a is"+a)
+// ++a
+// document.write("<br>after another incrementation, the new value of a is"+a)
+// --a
+// document.write("<br>after decrementation, the new value of a is"+a)
+// a--
+// document.write("<br>after another decrementation, the new value of a is"+a)
+
+// var a = 10
+// document.write("the value of a is "+a)
+// var b =20
+// document.write("the value of b is "+b)
+
+
+// var result = --a - --b + ++b + b--;
+// document.write("the value od result after--a - --b + ++b + b-- "+result)
+// document.write(result)
+
+// var userName = prompt("please enter the name");
+// if(userName){
+//     alert("hello "+userName,"!welcome to our website")
+
+// }
+
+// var num = prompt("enter the number")
+
+
+// for(i=1; 1<=10;i++){
+//     multiply= num*i
+//     document.write(multiply)
+// }
+
+// var sub1= prompt("name of first subject")
+// document.write("the name of 1st subject is \n"+sub1)
+// var sub2= prompt("the name f 2nd subect \n")
+// document.write("the name of 2nd subject is \n"+sub2)
+// var sub3 = prompt("name of 3rd subject\n")
+// document.write("the name of 3rd subject is\n "+sub3)
+
+// totalMarks = 300
+// document.write("the total marks is \n"+totalMarks)
+// var marksOf1st = prompt("obtaid in 1st subject\n")
+// document.write("the marks of 1st subject is \n"+marksOf1st)
+// var marksOf2nd = prompt("obtaid in 2nd subject\n")
+// document.write("the marks of 2nd subject is \n"+marksOf1st)
+// var marksOf3rd = prompt("obtaid in 3rd subject\n")
+// document.write("the marks of 3rd subject is \n"+marksOf1st)
+// persentage = ((marksOf1st+marksOf2nd+marksOf3rd)/totalMarks)*100
+// document.write("the persentage of all subject is"+persentage+"%")
+
+
+//if statement
+
+
+
+
+// var cityName= prompt("enter city name")
+//     myCity="karachi"
+// if(cityName==="karachi"){
+//     document.write("welcome to citt of light")
+// }
+// else{
+//     document.write("you are not from karachi")}
+
+// var gender= prompt("enter your gender")
+// if(gender==="male"){
+//     document.write("good morning")
+// }
+// else{
+//     document.write("good afternoon")}
+
+// var trafficSignal= prompt("enter your gender")
+// if(trafficSignal==="green"){
+//     document.write("move now")
+// }
+// else if(trafficSignal=="yellow"){
+//     document.write("ready to move")}
+//     else if(trafficSignal=="red"){
+//         document.write("stop moving")}
+    
+
+
+// var fuel =prompt("enter your fuelin ltrs")
+// if(fuel <= "1trs"){
+//     document.write("refill the tank")
+// }else{
+//     document.write("continue driving")}
+
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true")}
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{
+//     alert("given condition for variable b is false");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");}
+
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = 5000;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");}else
+// {alert ("the cost does not equal");}
+
+
+
+//  if (true){
+//     alert("True");
+//     }
+//     else if(false){
+//     alert("False");}
+
+
+    // if("car" < "cat"){
+    //     alert("car is smaller than cat");}
+
+
+//     var obtMarks=prompt("enter obtained marks")
+//     var totMarks = prompt("enter total marks")
+//     persentage = (obtMarks/totMarks)*100
+//     document.write("the persentage is "+persentage,+"%")
+//     if(persentage>=80){
+//         alert("Aone grade")
+//     }else if(persentage>=70)
+// {
+//     alert("A GRADE")
+// }
+// else if(persentage>=60){
+//     alert("B grade")
+
+// }
+// else{
+//     alert("fail")
+// }
+
+
+// var scretNumber= 79
+// guessNumber = prompt("guess the number")
+// if(scretNumber==="guessNumber"){
+//     alert("correct guess")
+// }
+// else if(guessNumber===scretNumber+1||guessNumber===scretNumber-1){
+//     alert("you are close to correct answer")
+// }
+// else{
+//     alert("wrong guess")
+//     alert("rigth answer is"+scretNumber)
+// }
+
+// var a = prompt("enter the number")
+// if(a % 3 ===0){
+//     alert("number is divisible by 3")
+// }
+// else{
+//     alert("number is not divisible by 3")
+// }
+
+
+// var temp = prompt("enter temprature")
+// if(temp>40){
+//     alert("it's hot outside")
+// }
+// else if(temp>30){
+//     alert("temperature is normal today")
+// }
+// else if (temp>20){
+//     alert("cool weather")
+// }
+// else if(temp >10){
+//     alert("wetaher is cold")
+// }
+// else{
+//     alert("weather is much cold")
+// }
+
+
+
+
+
+var a = prompt("enter number")
+var b = prompt("enter number")
+var operation=prompt("mathmatical operation")
+if(operation === +"" ){
+    alert(a+b)
+}
+ else if(operation==="-"){
+
+    alert(a-b)
+ } 
+ else if(operation=="*")
+ {
+    alert(a*b)
+ }
+ else if (operation==="/")
+ {
+    alert(a/b)
+ }
